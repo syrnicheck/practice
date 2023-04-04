5c54cd3a9666b05fd8aed473d34402dea41991f8
syrnicheck <105806186+syrnicheck@users.noreply.github.com>
2023-04-05 00:52:06 +0300

    add photo download

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import { Navigation } from './components/Navigation';
-import { HOME_PAGE_URL } from './constants/app';
+import { HOME_PAGE_URL,  SEARCH_PAGE_URL} from './constants/app';
 import { CategoryPage } from './pages/CategoryPage';
 import { MainPage } from './pages/MainPage';
 
@@ -10,7 +10,7 @@ function App() {
     <Navigation/>
     <Routes>
       <Route path={HOME_PAGE_URL} element={<MainPage/>}/>
-      <Route path="/search" element={<CategoryPage/>}/>
+      <Route path={SEARCH_PAGE_URL} element={<CategoryPage/>}/>
     </Routes>
     </>
   )
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/header.css';
+import { SEARCH_PAGE_URL } from "../constants/app";
 
 export function Navigation(){
     return(
@@ -21,9 +22,9 @@ export function Navigation(){
                             </button>
                         </div>
                         <nav className="header-nav">
-                            <Link className="link" to="/search">Explore</Link>
-                            <Link className="link" to="/search">Lisense</Link>
-                            <Link className="link" to="/search">Uplode</Link>
+                            <Link className="link" to={SEARCH_PAGE_URL}>Explore</Link>
+                            <Link className="link" to="https://www.pexels.com/ru-ru/license/" target="_blank">Lisense</Link>
+                            <Link className="link" to={SEARCH_PAGE_URL}>Uplode</Link>
                             <svg  className="link" fill="#4a4a4a" width="30px" height="30px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" stroke="#4a4a4a" transform="rotate(90)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M8 2.5a1.22 1.22 0 0 1 1.25 1.17A1.21 1.21 0 0 1 8 4.84a1.21 1.21 0 0 1-1.25-1.17A1.22 1.22 0 0 1 8 2.5zm0 8.66a1.17 1.17 0 1 1-1.25 1.17A1.21 1.21 0 0 1 8 11.16zm0-4.33a1.17 1.17 0 1 1 0 2.34 1.17 1.17 0 1 1 0-2.34z"></path></g></svg>
                             <button className="button">Join</button>
                         </nav>
